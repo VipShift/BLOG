4c4e0b4c4cd77e7bcb303b099cae72db9fba1d54
Vitalii <your-email@example.com>
2025-08-18 20:49:05 +0200

    Настроить проект

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+const Div = styled.div`
+  text-align: center;
+`;
+
+export function App() {
+  return (
+    <>
+      <Div>
+        <i className="fa fa-hand-spock-o"> </i>
+        <div>123</div>
+      </Div>
+    </>
+  );
+}
